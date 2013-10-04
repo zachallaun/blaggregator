acddb9ca522fde4ea3e44e039fabc46920e23063
Sasha Laundy <sasha.laundy@gmail.com>
2013-10-04 19:13:57 -0400

    Testing periodic tasks

--- home/tasks.py
+++ home/tasks.py
@@ -1,6 +1,8 @@
-from celery.task import Task
+from celery.task import Task, periodic_task
 import requests
 import os
+import logging
+from datetime import timedelta
 
 class ZulipNewPost(Task):
     """ Notify Zulip that there is a new blog post. """
@@ -24,3 +26,7 @@ class ZulipNewPost(Task):
         print "Here's what was sent to Zulip:", data['content']
         r = requests.post('https://humbughq-com-y3ee336dh1kn.runscope.net/api/v1/messages', data=data, auth=(email, key))
         return True
+        
+@periodic_task(run_every=timedelta(seconds=10))
+def print_fib():
+    logging.info('HELLO')
